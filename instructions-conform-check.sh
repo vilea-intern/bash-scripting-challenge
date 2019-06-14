44eb96b9047fdc461cc25e08a8f05204689fc8c3
vilea-intern <jeank@student.ethz.ch>
2019-06-14 15:17:50 +0200

    task 3

---
+++ instructions-conform-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Compute the hash of the file, select only the hash from the output
+hash1=$(md5sum ./randomfile | awk '{print $1;}')
+echo "file hash: ${hash1}"
+
+#Retrieve the previously computed checksum (We consider only the first word of the first line)
+hash2=$(cat ./randomfile-hash | head -n1 | awk '{print $1;}')
+echo "provided hash: ${hash2}"
+
+#Check if the strings are the same
+if [ "$hash1" = "$hash2" ]; then
+    echo "ok"
+else
+    echo "nok"
+fi
