065847f795660951469f8d00eef80a7e855697c1
vilea-intern <jeank @student.ethz.ch>
2019-06-15 09:52:49 +0200

    Handled non existing file

--- instructions-conform-check.sh
+++ instructions-conform-check.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+#Check if the file exists and is readable, reject it if not
+if [ ! -r ./randomfile ]; then
+    echo "nok"
+    exit 0
+fi
+
 #Compute the hash of the file, select only the hash from the output
 hash1=$(md5sum ./randomfile | awk '{print $1;}')
-#echo "file hash: ${hash1}"
 
 #Retrieve the previously computed checksum (We consider only the first word of the first line)
 hash2=$(cat ./randomfile-hash | head -n1 | awk '{print $1;}')
-#echo "provided hash: ${hash2}"
 
 #Check if the strings are the same
 if [ "$hash1" = "$hash2" ]; then
